edda68ea48b36cbe5dba9c0537c61f09281f7004
arman-borkhani <armanborkhani@gmail.com>
2023-09-08 16:58:50 +0330

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "c-green": "#3b8686",
+        "c-light-green": "#79bd9a",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    flex: {
+      "1": "1 1 0%",
+      "2": "2 2 0%",
+    },
+    container: {
+      center: true,
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
   plugins: [],
 }
 

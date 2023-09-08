83af752ae65e626cf80f42ee232fcff155b742c5
arman-borkhani <armanborkhani@gmail.com>
2023-09-08 10:54:48 +0330

    Initial commit

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient('https://qgquracomfodyknvtmym.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncXVyYWNvbWZvZHlrbnZ0bXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3NjQwMDUsImV4cCI6MTk5NjM0MDAwNX0.f8JLk4W77WYKITpztDnJ0AcA2slpDYq6G_VeooOuJvA');
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Page">
+    <h1>Home Page</h1>
+  </div>
+</template>

d5d29ffdb9665cbff82e8e5913eeb680374d6757
arman-borkhani <armanborkhani@gmail.com>
2023-09-14 19:16:15 +0330

    Add base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+    text-white bg-c-green duration-200 border-solid border-2 border-transparent hover:border-c-green
+    hover:bg-white hover:text-c-green">
+    <slot></slot>
+  </button>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import BaseButton from './components/BaseButton.vue';
 
 const app = createApp(App);
 
-app.use(router);
+app.component('BaseButton', BaseButton);
 
+app.use(router);
 app.mount('#app');

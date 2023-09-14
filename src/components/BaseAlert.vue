576bc235df5977858d6b3d182493010641376dd9
arman-borkhani <armanborkhani@gmail.com>
2023-09-14 19:26:06 +0330

    Add base alert component

---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,19 @@
+<template>
+  <div 
+    class="p-4 rounded-md flex items-center text-gray-800" 
+    :class="{'bg-red-100': messageType === 'danger', 'bg-green-100': messageType === 'success'}"
+  >
+    <i class="mr-2">
+      <IconDanger v-if="messageType === 'danger'" class="text-red-400" width="24" height="24"></IconDanger>
+      <IconSuccess v-else class="text-green-400" width="24" height="24"></IconSuccess>
+    </i>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import IconDanger from './icons/IconDanger.vue';
+import IconSuccess from './icons/IconSuccess.vue';
+
+defineProps(['messageType']);
+</script>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import BaseButton from './components/BaseButton.vue';
+import BaseAlert from './components/BaseAlert.vue';
 
 const app = createApp(App);
 
 app.component('BaseButton', BaseButton);
+app.component('BaseAlert', BaseAlert);
 
 app.use(router);
 app.mount('#app');

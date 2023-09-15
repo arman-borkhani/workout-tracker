e10f1776caf94460b8e9650b6022541730934c8d
arman-borkhani <armanborkhani@gmail.com>
2023-09-15 19:16:49 +0330

    Add loading for base button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,21 @@
 <template>
-  <button class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+  <button class="inline-flex align-middle py-2 px-6 rounded-sm self-start text-sm
     text-white bg-c-green duration-200 border-solid border-2 border-transparent hover:border-c-green
-    hover:bg-white hover:text-c-green">
+    hover:bg-white hover:text-c-green transition-all" :class="{'pointer-events-none bg-opacity-80': loading}"
+  >
+    <IconLoading v-if="loading" class="animate-spin -ml-2 mr-3 text-white" width="20" height="20"></IconLoading>
     <slot></slot>
   </button>
 </template>
+
+<script setup>
+import IconLoading from './icons/IconLoading.vue';
+
+defineProps({
+  loading: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+});
+</script>

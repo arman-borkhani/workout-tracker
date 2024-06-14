bbd3e95f6dc6341a9a10a2dd7530cf958dd7738b
arman-borkhani <armanborkhani@gmail.com>
2024-06-14 16:01:39 +0330

    Update supabase config

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient('https://qgquracomfodyknvtmym.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncXVyYWNvbWZvZHlrbnZ0bXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3NjQwMDUsImV4cCI6MTk5NjM0MDAwNX0.f8JLk4W77WYKITpztDnJ0AcA2slpDYq6G_VeooOuJvA');
+const supabaseUrl = 'https://qgquracomfodyknvtmym.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncXVyYWNvbWZvZHlrbnZ0bXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3NjQwMDUsImV4cCI6MTk5NjM0MDAwNX0.f8JLk4W77WYKITpztDnJ0AcA2slpDYq6G_VeooOuJvA';
+export const supabase = createClient(supabaseUrl, supabaseKey);
